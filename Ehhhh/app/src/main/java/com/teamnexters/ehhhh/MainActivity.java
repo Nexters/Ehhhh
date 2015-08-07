665e2f556ce395577a3be4a40647656b2fdcc30a
Shin Hyeon Sik <shs9589@gmail.com>
2015-08-07 11:54:08 +0900

    tab indicator color change

--- Ehhhh/app/src/main/java/com/teamnexters/ehhhh/MainActivity.java
+++ Ehhhh/app/src/main/java/com/teamnexters/ehhhh/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.teamnexters.ehhhh.activity.SearchActivity;
@@ -54,12 +53,10 @@ public class MainActivity extends AppCompatActivity {
 
                 @Override
                 public void onTabUnselected(TabLayout.Tab tab) {
-
                 }
 
                 @Override
                 public void onTabReselected(TabLayout.Tab tab) {
-
                 }
             });
 
