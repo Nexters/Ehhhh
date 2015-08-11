cde1c4960e90c0b600c575310c78044310542ba9
Shin Hyeon Sik <shs9589@gmail.com>
2015-08-11 23:12:22 +0900

    Revert "페북로그인 추가 작업 진행중"
    This reverts commit a044878af28419765716000e655e8c2e5a0316ec.

--- Ehhhh/app/src/main/java/com/teamnexters/ehhhh/Ehhhh.java
+++ Ehhhh/app/src/main/java/com/teamnexters/ehhhh/Ehhhh.java
@@ -3,7 +3,6 @@ package com.teamnexters.ehhhh;
 import android.app.Application;
 
 import com.parse.Parse;
-import com.parse.ParseFacebookUtils;
 
 /**
  * Created by HyeonSi on 2015-07-14.
@@ -17,6 +16,5 @@ public class Ehhhh extends Application {
         // Enable Local Datastore.
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, "GlRu30kOz11iiOVolSoLFNTq1eteictDJZQlMT0l", "9Fp73GbZ7NSWe7mD9W3oW8gcwN913j03UXAIJxBk");
-        ParseFacebookUtils.initialize(this);
     }
 }
--- Ehhhh/app/src/main/java/com/teamnexters/ehhhh/fragment/MyPageFragment.java
+++ Ehhhh/app/src/main/java/com/teamnexters/ehhhh/fragment/MyPageFragment.java
@@ -1,25 +1,15 @@
 package com.teamnexters.ehhhh.fragment;
 
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.parse.LogInCallback;
-import com.parse.ParseException;
-import com.parse.ParseFacebookUtils;
-import com.parse.ParseUser;
-import com.parse.SignUpCallback;
 import com.teamnexters.ehhhh.R;
-
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * Created by HyeonSi on 2015-07-22.
@@ -28,7 +18,6 @@ public class MyPageFragment extends Fragment {
 
     private static final String TAG = "MyPageFragement";
     Context mContext;
-    ProgressDialog dialog;
 
     public static MyPageFragment newInstance() {
         MyPageFragment fragment = new MyPageFragment();
@@ -53,50 +42,10 @@ public class MyPageFragment extends Fragment {
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                doSignUp();
+                Toast.makeText(mContext, "토스트", Toast.LENGTH_SHORT).show();
             }
         });
 
         return rootView;
     }
-
-
-    private void doSignUp() {
-        /*ParseUser user = new ParseUser();
-        user.setUsername("SIC");
-        user.setPassword("qqqq");
-        user.setEmail("user@email.com");
-
-        user.signUpInBackground(new SignUpCallback() {
-            @Override
-            public void done(ParseException e) {
-                if (e == null) {
-                    Toast.makeText(mContext, "성공!", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(mContext, "실패", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });*/
-
-        dialog = ProgressDialog.show(mContext, "", "Logging in....", true);
-
-        List<String> permissions = Arrays.asList("public_profile", "email");
-
-        ParseFacebookUtils.logInWithReadPermissionsInBackground(this, permissions, new LogInCallback() {
-            @Override
-            public void done(ParseUser parseUser, ParseException e) {
-                dialog.dismiss();
-                if (parseUser == null) {
-                    Log.d("MyApp", "Uh oh. The user cancelled the Facebook login.");
-                } else if (parseUser.isNew()) {
-                    Log.d("MyApp", "User signed up and logged in through Facebook!");
-                } else {
-                    Log.d("MyApp", "User logged in through Facebook!");
-                }
-            }
-        });
-
-    }
-
-
 }
